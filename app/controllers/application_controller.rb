c14aaae33ccabd2a143c0acf4b3215b00fb082fd
Sophie Gu <sophieqgu@gmail.com>
2019-09-15 21:35:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
     erb :show 
   end 
   
-  get '/articles/:id/edit'
+  get '/articles/:id/edit' do 
     @article = Article.find(params[:id])
     erb :edit 
   end 
